a4e58510b41047425d7cdd00dabceb41aa557c83
AkhilDande <venkataakhildande@gmail.com>
2025-01-10 23:53:19 +0530

    7th commit

--- src/Task11thJan2025/printf_Tableof9.java
+++ src/Task11thJan2025/printf_Tableof9.java
@@ -2,12 +2,12 @@ package Task11thJan2025;
 
 public class printf_Tableof9 {
     public static void main(String[] args) {
-        System.out.printf("9*1=%d\n", 9 * 1); // to print a table in next lin add \n before the string ends so that next statements gets printed in next lin
+        System.out.printf("9*1=%d\n", 9 * 1); // to print a statement in next line add \n before the string ends so that next statements gets printed in next line
         System.out.printf("9*2=%d\n", 9 * 2);  // use the %d to print the mathematical result inside the string and the input, after comma ex: 9*2 in this line
-        System.out.printf("9*3=%d\n", 9 * 3);  //
-        System.out.printf("9*4=%d\n", 9 * 4);
-        System.out.printf("9*5=%d\n", 9 * 5);
-        System.out.printf("9*6=%d\n", 9 * 6);
+        System.out.printf("9*3=%d\n", 9 * 3);  // Format of printf -> System.out.printf(locale, formatString, args)
+        System.out.printf("9*4=%d\n", 9 * 4);  // formatString = A string containing placeholders for the additional arguments indicating how to format them
+        System.out.printf("9*5=%d\n", 9 * 5);   // For Instance: System.out.printf("9*1=%d\n", 9 * 1);
+        System.out.printf("9*6=%d\n", 9 * 6);   // In the Above instance "9*1=%d\n" is the format of the string & "9*1" is the argument which is replaced by %d
         System.out.printf("9*7=%d\n", 9 * 7);
         System.out.printf("9*8=%d\n", 9 * 8);
         System.out.printf("9*9=%d\n", 9 * 9);
