ff1f2f4105148ef65c694eee8ee4a7b3c919a477
AkhilDande <venkataakhildande@gmail.com>
2025-01-13 22:55:57 +0530

    9th commit

---
+++ src/Task11thJan2025/Printfdoublexercise.java
@@ -0,0 +1,9 @@
+package Task11thJan2025;
+
+public class Printfdoublexercise {
+    public static void main(String[] args) {
+        double a = 9.99;
+        double b = 1.11;
+        System.out.printf("Sum of %f and %f is %f",a,b,a+b);
+    }
+}
